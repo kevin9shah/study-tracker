aee4b07bd8b35ae1ffd940c8572031be5a5fb7f9
kevin9shah <Kevinshah966@gmail.com>
2026-04-09 04:36:52 +0530

    imports

--- app/routes/deadline.py
+++ app/routes/deadline.py
@@ -1,19 +1,15 @@
-from fastapi import FastAPI, Depends, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter
 from app.db.databases import engine, Base, SessionLocal
 from app.models.user import User
-from app.models.subject import Subject
-from app.models.punishment import Punishment
-from app.models.progress import Progress
+
 from app.models.deadline import Deadline
-from app.models.couple import Couple
+
 from app.models.chapter import Chapter
 from sqlalchemy.orm import Session
 from app.db.databases import get_db
-from datetime import datetime
-from enum import Enum
-from pydantic import BaseModel
+
 from app.schemas.deadline import DeadlineCreate
-import random
+
 
 router = APIRouter(prefix="/deadline", tags=["Deadline"])
 
--- app/schemas/chapter.py
+++ app/schemas/chapter.py
@@ -1,16 +1,3 @@
-from fastapi import FastAPI, Depends, HTTPException, APIRouter
-from app.db.databases import engine, Base, SessionLocal
-from app.models.user import User
-from app.models.subject import Subject
-from app.models.punishment import Punishment
-from app.models.progress import Progress
-from app.models.deadline import Deadline
-from app.models.couple import Couple
-from app.models.chapter import Chapter
-from sqlalchemy.orm import Session
-from app.db.databases import get_db
-from datetime import datetime
-from enum import Enum
 from pydantic import BaseModel
 
 
